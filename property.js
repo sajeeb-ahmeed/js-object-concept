fcbce5a97d0236765576b998b2b0f53c9a6a384a
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-26 13:22:26 +0600

    use keys & valuse method to acces object proterty and propertys values

---
+++ property.js
@@ -0,0 +1,14 @@
+const student = {
+    name: ' Tom Alex',
+    money: 5000,
+    age: 21,
+    subject: ' Mathmaticies',
+    color: ' black'
+}
+
+// get keys of object useing keys method
+const keys = Object.keys(student);
+console.log(keys);
+//get values useing of values method 
+const value = Object.values(student);
+console.log(value);
