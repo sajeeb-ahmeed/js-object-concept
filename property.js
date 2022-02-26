681212c2e7d03a3892e91cba2dce02300249378f
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-26 13:33:33 +0600

    delete object property => use delete objectName.property

--- property.js
+++ property.js
@@ -12,6 +12,10 @@ console.log(keys);
 //get values useing of values method 
 const value = Object.values(student);
 console.log(value);
-// pairs =    its return a two dimentional array 
+// pairs =    its return a two dimentional array // Returns an array of key/values of the enumerable properties of an object
 const pair = Object.entries(student);
 console.log(pair);
+
+// delete 
+delete student.color;
+console.log(student);
